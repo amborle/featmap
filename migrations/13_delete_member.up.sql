f735627eec58d261bc38de132eab9db23d92df71
amb <amb@DESKTOP-7EK7T20.localdomain>
2020-01-21 22:37:10 +0100

    fixing bug

---
+++ migrations/13_delete_member.up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE features DROP COLUMN created_by;
+ALTER TABLE milestones DROP COLUMN created_by;
+ALTER TABLE projects DROP COLUMN created_by;
+ALTER TABLE workflows DROP COLUMN created_by;
+ALTER TABLE subworkflows DROP COLUMN created_by;
